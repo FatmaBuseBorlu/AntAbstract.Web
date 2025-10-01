d5dca6867cc56d8019b260837a76c3e70444200c
Fatma Buse Borlu <fatmabuse.120@gmail.com>
2025-10-01 22:31:26 +0300

    .

---
+++ AntAbstract.Domain/Entities/Registration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AntAbstract.Domain.Entities
+{
+    public class Registration
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        // Kaydı yapan kullanıcı
+        public string AppUserId { get; set; }
+        [ForeignKey("AppUserId")]
+        public AppUser AppUser { get; set; }
+
+        // Hangi kongreye kayıt yapıldığı
+        public Guid ConferenceId { get; set; }
+        [ForeignKey("ConferenceId")]
+        public Conference Conference { get; set; }
+
+        // Hangi kayıt tipini seçtiği
+        public Guid RegistrationTypeId { get; set; }
+        [ForeignKey("RegistrationTypeId")]
+        public RegistrationType RegistrationType { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+
+        // Ödeme durumu
+        public bool IsPaid { get; set; } = false;
+        public DateTime? PaymentDate { get; set; }
+
+        // Ödeme sağlayıcısından gelen işlem ID'si (örn: Stripe'ın 'charge_id'si)
+        public string? PaymentTransactionId { get; set; }
+    }
+}
---
+++ AntAbstract.Domain/Entities/RegistrationType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AntAbstract.Domain.Entities
+{
+    public class RegistrationType
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } // Örn: "Öğrenci Kaydı", "Tam Katılım"
+
+        [StringLength(500)]
+        public string Description { get; set; } // Örn: "Gala yemeği dahildir."
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; } // Kayıt ücreti
+
+        [Required]
+        [StringLength(10)]
+        public string Currency { get; set; } = "TRY"; // Para birimi (USD, EUR, TRY)
+
+        public Guid ConferenceId { get; set; }
+        [ForeignKey("ConferenceId")]
+        public Conference Conference { get; set; }
+    }
+}
