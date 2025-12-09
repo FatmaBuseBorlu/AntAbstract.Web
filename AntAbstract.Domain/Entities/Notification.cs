d8e14952a36f305477b97e4af7a9c33088f8a5b4
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2025-12-09 17:18:49 +0300

    .

---
+++ AntAbstract.Domain/Entities/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AntAbstract.Domain.Entities
+{
+    public class Notification
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        [Required]
+        public string Title { get; set; }  
+        public string Message { get; set; } 
+        public string Link { get; set; }    
+        public bool IsRead { get; set; } = false; 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- AntAbstract.Infrastructure/Context/AppDbContext.cs
+++ AntAbstract.Infrastructure/Context/AppDbContext.cs
@@ -3,12 +3,6 @@ using AntAbstract.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AntAbstract.Infrastructure.Context
 {
@@ -17,27 +11,31 @@ namespace AntAbstract.Infrastructure.Context
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Tenant> Tenants => Set<Tenant>();
-        public DbSet<Conference> Conferences => Set<Conference>();
-        public DbSet<Submission> Submissions => Set<Submission>();
-        public DbSet<Reviewer> Reviewers { get; set; }
-        public DbSet<ReviewAssignment> ReviewAssignments { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-        public DbSet<Message> Messages { get; set; }
-        public DbSet<ScientificField> ScientificFields { get; set; }
-        public DbSet<CongressType> CongressTypes { get; set; }
-        public DbSet<Session> Sessions { get; set; }
-        public DbSet<ReviewForm> ReviewForms { get; set; }
-        public DbSet<ReviewCriterion> ReviewCriteria { get; set; }
-        public DbSet<ReviewAnswer> ReviewAnswers { get; set; }
-        public DbSet<RegistrationType> RegistrationTypes { get; set; }
-        public DbSet<Registration> Registrations { get; set; }
-        public DbSet<Payment> Payments { get; set; }
+
+        // --- TEMEL TABLOLAR ---
+        public DbSet<Tenant> Tenants => Set<Tenant>(); // Çoklu Kongre Altyapısı
+        public DbSet<Conference> Conferences => Set<Conference>(); // Kongreler
+        public DbSet<Submission> Submissions => Set<Submission>(); // Bildiriler
+        public DbSet<SubmissionAuthor> SubmissionAuthors { get; set; } // Bildiri Yazarları
+        public DbSet<SubmissionFile> SubmissionFiles { get; set; } // Bildiri Dosyaları
+
+        // --- DEĞERLENDİRME & HAKEMLİK ---
+        public DbSet<ReviewAssignment> ReviewAssignments { get; set; } // Hakem Atamaları
+        public DbSet<Review> Reviews { get; set; } // Puanlama ve Yorumlar
+
+        // --- İLETİŞİM & KAYIT ---
+        public DbSet<Message> Messages { get; set; } // Mesajlaşma
+        public DbSet<Notification> Notifications { get; set; } // Bildirimler (Yeni Ekledik!)
+        public DbSet<Registration> Registrations { get; set; } // Kongre Kayıtları
+
+        // --- PROGRAM & DİĞER ---
+        public DbSet<Session> Sessions { get; set; } // Kongre Programı (İleride Kullanılacak)
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            // BİLDİRİ İLİŞKİLERİ
             builder.Entity<Submission>()
                 .HasMany(s => s.ReviewAssignments)
                 .WithOne(ra => ra.Submission)
@@ -50,12 +48,26 @@ namespace AntAbstract.Infrastructure.Context
                 .HasForeignKey(s => s.AuthorId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            builder.Entity<Submission>()
+                .HasMany(s => s.SubmissionAuthors)
+                .WithOne(sa => sa.Submission)
+                .HasForeignKey(sa => sa.SubmissionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Submission>()
+                .HasMany(s => s.Files)
+                .WithOne(f => f.Submission)
+                .HasForeignKey(f => f.SubmissionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // HAKEM ATAMA İLİŞKİSİ
             builder.Entity<ReviewAssignment>()
-                .HasOne(ra => ra.AppUser)
+                .HasOne(ra => ra.AppUser) // Hakem (AppUser)
                 .WithMany()
                 .HasForeignKey(ra => ra.ReviewerId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            // MESAJLAŞMA İLİŞKİSİ
             builder.Entity<Message>(entity =>
             {
                 entity.HasOne(m => m.Sender)
@@ -69,40 +81,19 @@ namespace AntAbstract.Infrastructure.Context
                     .OnDelete(DeleteBehavior.Restrict);
             });
 
+            // PROGRAM (SESSION) İLİŞKİSİ
             builder.Entity<Session>()
                 .HasOne(s => s.Conference)
                 .WithMany()
                 .HasForeignKey(s => s.ConferenceId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            builder.Entity<Review>()
-                .HasMany(r => r.Answers)
-                .WithOne(a => a.Review)
-                .HasForeignKey(a => a.ReviewId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            builder.Entity<ReviewForm>()
-                .HasMany(f => f.Criteria)
-                .WithOne(c => c.Form)
-                .HasForeignKey(c => c.FormId)
-                .OnDelete(DeleteBehavior.Cascade);
-
+            // KAYIT İLİŞKİSİ
             builder.Entity<Conference>()
                 .HasMany<Registration>()
                 .WithOne(r => r.Conference)
                 .HasForeignKey(r => r.ConferenceId)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            builder.Entity<Conference>()
-                .HasMany<RegistrationType>()
-                .WithOne(rt => rt.Conference)
-                .HasForeignKey(rt => rt.ConferenceId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            builder.Entity<Payment>()
-                .Property(p => p.Amount)
-                .HasColumnType("decimal(18, 2)");
-
         }
     }
 }
